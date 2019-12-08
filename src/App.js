39c7285266883d76bfd8c92b2587a5d9e7a1e373
wright1 <prosperwright@yahoo.co.uk>
2019-12-08 11:43:55 +0000

    righthand btn works on fullwidth view

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ const App = () => {
   const [items, setItems] = useState([]);
   // this varible keeps a reference of which image we are seeing
   const slideRef = useRef(8);
+  const count = useRef(1);
+  let width = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
   useEffect(() => {
     const data = "https://api.myjson.com/bins/ea8gg";
     fetch(data)
@@ -20,19 +25,50 @@ const App = () => {
       .then(json => setItems(json));
   }, []);
   const handleRightClick = e => {
+    console.log(width)
+    //mobile view
+    const imgList = e.target.parentNode.previousSibling.childNodes
+    if (width < 1025){
     if( slideRef.current === 0) return;
     // this give me a node list of all the images - first image is last on list
-    const imgList = e.target.parentNode.previousSibling.childNodes[slideRef.current];
+    ;
     console.log(slideRef)
-    imgList.style.transform = `translateX(${700}px)`;
+    imgList[slideRef.current].style.transform = `translateX(${700}px)`;
     slideRef.current--;
+    } else {
+      //console.log(count.current);
+      if(count.current === 7) return;
+      let current = imgList[count.current];
+      let added = count.current +2;
+      // the center image is always showing
+      current.style.display = "initial";
+      // as we move forward remove the very first image
+      if (count.current > 1) imgList[count.current - 2].style.display = "none";
+      // remove left image
+      imgList[count.current - 1].style.display = "none";
+      imgList[count.current + 1].style.display = "initial";
+      // add new image
+      imgList[added].style.display = "initial"
+      count.current+= 2;
+    }
   };
   const handleLeftClick = e => {
+    // mobile view
+    const imgList = e.target.parentNode.previousSibling.childNodes
+    if(width < 1025){
     if(slideRef.current === 8) return;
     slideRef.current++;
-    const imgList = e.target.parentNode.previousSibling.childNodes[slideRef.current];
-    imgList.style.transform = `translateX(${-0.5}px)`;
-    
+    imgList[slideRef.current].style.transform = `translateX(${-0.5}px)`;
+    } else {
+      count.current -= 2;
+      const current = imgList[count.current];
+      if(count.current < 1) count.current = 1;
+  
+      console.log(count.current, imgList)
+      imgList[count.current-1].style.display = "initial";
+      current.style.display = "initial";
+      imgList[count.current +1].style.display = "initial"; 
+    }
     
   }
 
